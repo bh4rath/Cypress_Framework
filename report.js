aeedbe30f40384e7da83b60ef0aee16e042c48f5
Bharath Selvam <bharathgates33@gmail.com>
2020-06-08 20:51:51 +0530

    cucumber reports added

---
+++ report.js
@@ -0,0 +1,25 @@
+const fs = require('fs-extra');
+
+const dir = './cucumber_report/html';
+
+if(fs.existsSync(dir)){
+    fs.removeSync(dir);
+}
+
+fs.mkdirSync(dir);
+
+const reporter = require('cucumber-html-reporter');
+
+const options = {
+    theme: 'bootstrap',
+    jsonDir: 'cucumber_report',
+    output: 'cucumber_report/html/cucumber_report.html',
+    reportSuiteAsScenarios: true,
+    scenarioTimestamp: true,
+    lauchReport: true,
+    metadata: {
+
+    }
+};
+
+reporter.generate(options);
